5904936fff38503ede8a78817d3347086f2a2485
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-11-22 07:28:58 -0700

    Use different annotation

--- src/test/java/com/bmuschko/java11/var/LocalVariableTypeInferenceTest.java
+++ src/test/java/com/bmuschko/java11/var/LocalVariableTypeInferenceTest.java
@@ -2,7 +2,7 @@ package com.bmuschko.java11.var;
 
 import org.junit.jupiter.api.Test;
 
-import javax.annotation.Nonnull;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -15,7 +15,7 @@ public class LocalVariableTypeInferenceTest {
         List<String> months = List.of("January", "February", "March");
         List<String> filteredMonths = months
                 .stream()
-                .filter(Predicate.not((@Nonnull var month) -> month.startsWith("M")))
+                .filter(Predicate.not((@NotNull var month) -> month.startsWith("M")))
                 .collect(Collectors.toList());
         assertEquals(List.of("January", "February"), filteredMonths);
     }
